9ca8a5427675827bc797efeaca2429112dc460cc
Harashil <1491109@senati.pe>
2024-06-28 10:40:28 -0500

    Cambio..

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: process.env.NODE_ENV === 'development'
+          ? 'http://127.0.0.1:8000/:path*' // Proxy to FastAPI in development
+          : '/api/:path*' // Proxy to FastAPI in production
+      }
+    ];
+  }
   }
   
   
