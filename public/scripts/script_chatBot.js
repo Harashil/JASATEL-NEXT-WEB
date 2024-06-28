0b87a18d3168140f0a54c42d2f0251a637e85c7f
Harashil <1491109@senati.pe>
2024-06-28 12:00:09 -0500

    cambio 3

--- public/scripts/script_chatBot.js
+++ public/scripts/script_chatBot.js
@@ -96,7 +96,7 @@ function sendMessage() {
   console.log(userMessage);
   console.log(userMessage.textContent);
   if (userMessage.textContent !== "") {
-    fetch("/app/api/main/pregunta", {
+    fetch("/api/pregunta", {
       method: "POST",
       headers: {
         "Content-Type": "application/json", // Especifica el tipo de contenido como JSON
